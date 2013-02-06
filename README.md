0c569b1b0e2aaf205c12ecd4eac9012a2fe52af7
fred <fred.matigot@gmail.com>
2013-02-06 15:10:15 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-ideep
-=====
+Service web
+===========
+
+Ce service associe l'ID d'un sensor à :
+  - son eep
+  - son nom (== description abrégée)
+  - sa description longue
+  - sa liste de types
+
+Pour rajouter des associations possibles il suffit de modifier les deux dictionnaires contenus dans `app/ideep.py`.
+
+Ce service tourne à l'adresse http://atom.fspot.org/ideep/.
+
+Exemple d'utilisation :
+  - Requête http GET sur `http://atom.fspot.org/ideep/42`
+  - Réponse (format JSON) =
+```javascript
+{
+	"found": true, 
+	"eep": "05-02-01"
+	"name": "Interrupteur", 
+	"description": "Double interrupteur blanc", 
+	"datatypes": [
+		"switch", 
+		"switch", 
+		null, 
+		null, 
+		null, 
+		null
+	], 
+}
+```
+
+Attention, dans l'url, 42 est passé en tant que **chaîne**.
+C'est pourquoi dans le dictionnaire d'association `id => eep`, l'id est également une **chaîne**.
+
+Si l'ID est inconnu, la réponse sera toujours `{"found": false}`
+
+Pour faire tourner le service chez vous :
+```bash
+$ git clone https://github.com/fspot/ideep.git
+$ cd ideep
+$ virtualenv venv
+$ source venv/bin/activate
+$ pip install flask gunicorn
+$ cd app
+$ gunicorn ideep:app
+```
+
+Cela le fera tourner sur le port 8000. Vous pouvez tester à `http://localhost:8000/ideep/`
+
+Conf nginx, chez moi j'ai simplement rajouté :
+```bash
+location /ideep/ {
+    proxy_pass http://127.0.0.1:8000;
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+}
+```
---
+++ app/ideep.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from flask import Flask, jsonify
+from werkzeug.contrib.fixers import ProxyFix
+
+app = Flask(__name__)
+null = None  # au cas ou un 'null' traine...
+
+# Ici on mettra tout ce qui définit un eep.
+# Ce dictionnaire est censé être figé,
+# tant que de nouveaux modèles de capteurs de sortent pas.
+eep_to_info = {
+    '07-02-01': {
+        "eep": "07-02-01",
+        "name": "Capteur de température (A)",
+        "description": "Capteur de température, modèle A",
+        "datatypes": ["temperature", None, None, None, None, None]
+    },
+    '07-02-04': {
+        "eep": "07-02-04",
+        "name": "Capteur de température (B)",
+        "description": "Capteur de température, modèle B",
+        "datatypes": ["temperature", None, None, None, None, None]
+    },
+    '07-02-07': {
+        "eep": "07-02-07",
+        "name": "Capteur de température (C)",
+        "description": "Capteur de température, modèle C",
+        "datatypes": ["temperature", None, None, None, None, None]
+    },
+    '05-02-01': {
+        "eep": "05-02-01",
+        "name": "Interrupteur",
+        "description": "Double interrupteur blanc",
+        "datatypes": ["switch", "switch", None, None, None, None]
+    },
+    '06-00-01': {
+        "eep": "06-00-01",
+        "name": "Contacteur de porte/fenêtre",
+        "description": "Contacteur de porte/fenêtre blanc",
+        "datatypes": ["contact", None, None, None, None, None]
+    },
+    '07-08-02': {
+        "eep": "07-08-02",
+        "name": "Capteur multifonctions (A)",
+        "description": "Capteur multifonctions (température, présence, luminosité), modèle A",
+        "datatypes": ["temperature", "occupancy", None, None, None, None]
+    },
+}
+
+# Ce dictionnaire en revanche, sera à modifier à chaque nouvel ID
+# Il va sans dire que dans un cas réel, il y aurait probablement une astuce
+# arithmétique pour passer d'un id à un eep.
+# (sinon il y aurait beaucoup trop de données)
+# Mais pour ce tp on s'en contentera.
+id_to_eep = {
+    '42': '05-02-01',  # <== keys must be strings !
+    'test': '05-02-01',
+}
+
+
+# DO NOT MODIFY BELOW
+def id_to_info(idy):
+    if idy in id_to_eep:
+        eep = id_to_eep[idy]
+        if eep in eep_to_info:
+            return eep_to_info[eep]
+    return None
+
+
+@app.route('/ideep/')
+def ideep_index():
+    ''' racine '''
+    return jsonify({'msg': 'It Works :)', 'status': 'success'})
+
+
+@app.route('/ideep/<idy>')
+def ideep_mapping(idy):
+    obj = id_to_info(idy)
+    if obj is None:
+        obj = {'found': False}
+    else:
+        obj['found'] = True
+    return jsonify(obj)
+
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
